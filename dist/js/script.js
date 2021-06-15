5bbc548fab6da3842977b549e029d3785a4159bb
azesm <azesm@yahoo.com>
2021-06-15 18:10:31 +0300

    3.3 tabs 11.26

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,28 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelectorAll('.tabheader__items');
 
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+    tabs.forEach(item => {
+      item.classList.remove('.tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i) {
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('.tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent(0);
+});
 
 /***/ })
 
