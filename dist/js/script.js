bba44521cab0ec95b48f0a24485a9790f88b82fa
azesm <azesm@yahoo.com>
2021-06-30 10:42:32 +0300

    half part of timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -133,6 +133,35 @@ window.addEventListener('DOMContentLoaded', function () {
       });
     }
   }); //__________________________________Timer________________________
+
+  const deadline = '2021-08-29';
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor(t / (1000 * 60 * 60) % 24),
+          minutes = Math.floor(t / 1000 / 60 % 60),
+          seconds = Math.floor(t / 1000 % 60);
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds');
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+    }
+  }
 });
 
 /***/ })
