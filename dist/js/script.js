61d33cc46771463380da69bc5d3092d4f5b90d55
azesm <azesm@yahoo.com>
2021-07-16 18:35:08 +0300

    3.14End

--- dist/js/script.js
+++ dist/js/script.js
@@ -233,12 +233,13 @@ window.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', showModalByScroll); //Используем классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 77;
       this.changeToRUB();
@@ -250,8 +251,15 @@ window.addEventListener('DOMContentLoaded', function () {
 
     render() {
       const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-            <div class = "menu__item" >
                 <img src = ${this.src} alt = ${this.alt}>
                 <h3 class = "menu__item-subtitle" >${this.title}</h3>
                 <div class = "menu__item-descr" >${this.descr}</div>
@@ -260,7 +268,6 @@ window.addEventListener('DOMContentLoaded', function () {
                     <div class = "menu__item-cost"> Цена: </div>
                     <div class = "menu__item-total" > <span>${this.price}</span> руб./день</div>
                 </div>
-            </div>
             `;
       this.parent.append(element);
     }
--- src/js/main.js
+++ src/js/main.js
@@ -167,12 +167,13 @@ window.addEventListener('DOMContentLoaded', function () {
     //Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 77;
             this.changeToRUB();
@@ -184,8 +185,15 @@ window.addEventListener('DOMContentLoaded', function () {
 
         render() {
             const element = document.createElement('div');
+            
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
             element.innerHTML = `
-            <div class = "menu__item" >
                 <img src = ${this.src} alt = ${this.alt}>
                 <h3 class = "menu__item-subtitle" >${this.title}</h3>
                 <div class = "menu__item-descr" >${this.descr}</div>
@@ -194,7 +202,6 @@ window.addEventListener('DOMContentLoaded', function () {
                     <div class = "menu__item-cost"> Цена: </div>
                     <div class = "menu__item-total" > <span>${this.price}</span> руб./день</div>
                 </div>
-            </div>
             `;
             this.parent.append(element);
         }
@@ -206,7 +213,7 @@ window.addEventListener('DOMContentLoaded', function () {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         11,
-        '.menu .container'
+        '.menu .container'        
     ).render();
 
     new MenuCard(
